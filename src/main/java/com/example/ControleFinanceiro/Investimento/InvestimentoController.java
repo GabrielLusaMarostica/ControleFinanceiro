7db50cc2f6218dc5164a311a33ab3719fba36589
Gabriel Lusa Marostica <gabriellmarostica@gmail.com>
2025-09-05 13:31:02 -0300

    feat: criar endpoints para Investimentos e Transações

--- src/main/java/com/example/ControleFinanceiro/Investimento/InvestimentoController.java
+++ src/main/java/com/example/ControleFinanceiro/Investimento/InvestimentoController.java
@@ -1,11 +1,11 @@
 package com.example.ControleFinanceiro.Investimento;
 
+import jdk.javadoc.doclet.Reporter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/investimento")
@@ -17,8 +17,48 @@ public class InvestimentoController {
     }
 
     @PostMapping("/criar/{id}")
-    public ResponseEntity<InvestimentoModel> criarInvestimento(@PathVariable Long id, InvestimentoModel investimentoModel){
+    public ResponseEntity<InvestimentoModel> criarInvestimento(@PathVariable Long id, @RequestBody InvestimentoModel investimentoModel){
         return ResponseEntity.status(HttpStatus.CREATED).body(investimentoService.criarInvestimento(id, investimentoModel));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<InvestimentoModel>> listarInvesimentos(){
+        return ResponseEntity.ok()
+                .body(investimentoService.listarInvestimentos());
+    }
+
+    @GetMapping("/listarPorId/{id}")
+    public ResponseEntity<InvestimentoModel> listarInvestimentoPorId(@PathVariable Long id){
+        if(investimentoService.listarInvestimentoPorId(id) != null){
+            return ResponseEntity.ok(investimentoService.listarInvestimentoPorId(id));
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(null);
+        }
+    }
+
+    @PutMapping("/alterar/{id}")
+    public ResponseEntity<InvestimentoModel> alterarInvestimento(@PathVariable Long id, @RequestBody InvestimentoModel investimentoModel){
+        if(investimentoService.listarInvestimentoPorId(id) != null){
+            investimentoService.alterarInvestimento(id, investimentoModel);
+            return ResponseEntity.ok(investimentoService.listarInvestimentoPorId(id));
+        }
+        else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(null);
+        }
+    }
+
+    @DeleteMapping("/deletar/{id}")
+    public ResponseEntity<String> deletarInvestimento(@PathVariable Long id){
+        if(investimentoService.listarInvestimentoPorId(id) != null){
+            investimentoService.deletarInvestimento(id);
+            return ResponseEntity.ok("Investimento de id " + id + " foi deletado com sucesso");
+        }
+        else {
+            return ResponseEntity.ok("Não foi encontrado nenhum investimento com este id");
+        }
     }
 
 }
--- src/main/java/com/example/ControleFinanceiro/Transacao/TransacaoController.java
+++ src/main/java/com/example/ControleFinanceiro/Transacao/TransacaoController.java
@@ -1,8 +1,11 @@
 package com.example.ControleFinanceiro.Transacao;
 
+import com.example.ControleFinanceiro.Investimento.InvestimentoModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/conta")
@@ -15,8 +18,24 @@ public class TransacaoController {
         this.transacaoService = transacaoService;
     }
 
-    @PostMapping("/criar/{contaId}")
-    public ResponseEntity<TransacaoModel> criar(@PathVariable Long contaId, @RequestBody TransacaoModel transacao) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.criarTransacao(contaId, transacao));
+    @PostMapping("/criar/{id}")
+    public ResponseEntity<TransacaoModel> criar(@PathVariable Long id, @RequestBody TransacaoModel transacao) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(transacaoService.criarTransacao(id, transacao));
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<TransacaoModel>> listarTransacoes(){
+        return ResponseEntity.ok(transacaoService.listarTransacoes());
+    }
+
+    @GetMapping("/listar/{id}")
+    public ResponseEntity<TransacaoModel> listarTransacaoPorId(@PathVariable Long id){
+        if(transacaoService.listarPorId(id) != null){
+            return ResponseEntity.ok(transacaoService.listarPorId(id));
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(null);
+        }
     }
 }
